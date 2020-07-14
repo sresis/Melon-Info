773513d7256fdda580aec99a6d07e8f027b32d26
Stephanie Resis <sresis@umich.edu>
2020-07-14 04:43:40 +0000

    added nested dictionary functionality

--- melon_info.py
+++ melon_info.py
@@ -4,11 +4,12 @@
 from melons import melon_names, melon_seedlessness, melon_prices
 
 
+
 def print_melon(name, seedless, price, rind_color = 'tan', weight = 2, *traits):
     """Print each melon with corresponding attribute information."""
     melon_info = {}
     melon_traits = {}
-    melon_traits[price] = 1
+    
     #melon_traits[seedless] = True
     #melon_traits[rind_color] = rind_color
     #melon_traits[weight] = weight
@@ -24,7 +25,21 @@ def print_melon(name, seedless, price, rind_color = 'tan', weight = 2, *traits):
     	'Seedlessness': melon_seedlessness[melon],
     	'Rind Color': rind_color,
     	'Weight': weight}
-    print(melon_traits)
+
+    # inner function to pring all items seperately
+    def print_all_traits(melon_traits):
+    	for key, value in melon_traits.items():
+    		if type(value) is dict:
+    			print(key.upper())
+    			print_all_traits(value)
+    		else:
+    			print(f'{key}: {value}')
+
+    print_all_traits(melon_traits)
+
+    #	for item in melon_traits.items():
+    #		print(item)
+    	
     """for melon in melon_names:
     	for item in melon_traits:
     		print((value, key in melon_traits.items())"""
